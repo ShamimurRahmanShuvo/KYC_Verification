192a615265866ef1c26f663ef0f8f1d1da734c04
ShamimurRahmanShuvo <shamimur.shuvo@komute.ca>
2026-04-24 10:55:44 -0400

    Initial RBAC

--- app/core/auth.py
+++ app/core/auth.py
@@ -1,7 +1,17 @@
 from jose import jwt
+from passlib.context import CryptContext
 from datetime import datetime, timedelta
 
 SECRET = "supersecret"
+pwd=CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+def hash_password(password: str):
+    return pwd.hash(password)
+
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd.verify(plain_password, hashed_password)
 
 
 def create_token(data: dict):
--- app/core/config.py
+++ app/core/config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 
 class Settings:
-    DATABASE_URL = "sqlite:///./kyc.db"
+    DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./kyc1.db")
     SECRET_KEY = os.getenv("SECRET_KEY", "supersecret")
 
 
